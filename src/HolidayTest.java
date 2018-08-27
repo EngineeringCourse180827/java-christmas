ed69f81e91d61be811cac8dd306d551878e2da22
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-08-27 11:39:44 +0800

    :recycle: add more tests and refactoring

--- src/HolidayTest.java
+++ src/HolidayTest.java
@@ -6,12 +6,35 @@ import static org.junit.Assert.assertEquals;
 
 public class HolidayTest {
 
-    @Test
-    public void today_is_Christmas() {
-        FakeHolidayForTest holiday = new FakeHolidayForTest();
-        holiday.setToday(LocalDate.parse("2018-12-25"));
+    private FakeHolidayForTest holiday = new FakeHolidayForTest();
 
-        assertEquals("Merry Christmas", holiday.isTodayChristmas());
+    @Test
+    public void today_is_christmas_dec_25() {
+        givenToday("12-25");
+
+        responseShouldBe("Merry Christmas");
+    }
+
+    @Test
+    public void today_is_christmas_dec_24() {
+        givenToday("12-24");
+
+        responseShouldBe("Merry Christmas");
+    }
+
+    @Test
+    public void today_is_not_christmas() {
+        givenToday("09-01");
+
+        responseShouldBe("Not X'mas");
+    }
+
+    private void responseShouldBe(String expected) {
+        assertEquals(expected, holiday.isTodayChristmas());
+    }
+
+    private void givenToday(String monthDay) {
+        holiday.setToday(LocalDate.parse("2018-" + monthDay));
     }
 
     class FakeHolidayForTest extends Holiday {
